96067357e96dfea131cbd3e09b73b3c8e0a1bf54
ioreshkin <fennonplay@gmail.com>
2024-05-12 16:14:08 +0500

    132

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './reset.css';
 import './components/pages/mainPage/mainPage';
+import Navigation from "./components/navigation/navigation";
 import MainPage from './components/pages/mainPage/mainPage';
 import Help from './components/popup/help/help';
 import { useState } from 'react';
@@ -8,6 +9,7 @@ function App() {
   const [helpActive, setHelpActive] = useState(true);
   return (
     <div className="App">
+      <Navigation/>
       <MainPage/>
       <Help active={helpActive} setActive={setHelpActive}/>
     </div>
