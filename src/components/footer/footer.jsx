8488701df8879b4fa860ea3cfc893383b7ee7f28
ioreshkin <fennonplay@gmail.com>
2024-05-12 14:02:10 +0500

    123

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import cl from "./footer.module.css";
 
 const Footer = () => {
     return (
-        <footer className="footer">
-            <div className="footer__content">
+        <footer className={cl.footer}>
+            <div className={cl.footer__content}>
                 <img
                     src="/logo.png"
                     alt="Логотип"
@@ -15,16 +16,16 @@ const Footer = () => {
                         height: "45px",
                     }}
                 />
-                <div className="footer__contacts">
+                <div className={cl.footer__contacts}>
                     <div>+7(902)471-65-14</div>
                     <div>example@mail.ru</div>
                 </div>
             </div>
-            <div className="footer__text">
+            <div className={cl.footer__text}>
                 <div>©2024 La JuliaGoshaVaryaIlya</div>
                 <div>Easy FIT</div>
             </div>
-            <div className="footer__contacts">Contacts</div>
+            <div className={cl.footer__contacts}>Contacts</div>
         </footer>
     );
 }
