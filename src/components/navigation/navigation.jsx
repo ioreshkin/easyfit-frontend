6930068c1114031f4164955808f579722be6da08
ioreshkin <fennonplay@gmail.com>
2024-05-12 13:04:42 +0500

    123

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import cl from "./navigation.module.css";
+import LangSwitch from "../buttons/langSwitch/langSwitch";
+import Hyperlink from "../buttons/hyperlink/hyperlink";
+import logo from "../../images/blackLogo.png";
 
 const Navigation = () => {
     return (
         <div className={cl.navigation}>
-            <a href=""></a>
-            <div></div>
-            
+            <a href="#">
+                <img src={logo}/>
+            </a>
+            <div className={cl.links}>
+                <Hyperlink text="Программы"/>
+                <Hyperlink text="Упражнения"/>
+                <Hyperlink text="Помощь"/>
+                <Hyperlink text="О нас"/>
+            </div>
+            <LangSwitch/>
         </div>
     )
 }
