f0af83f1778d35945778f4e9d695f7a786a0f01c
ioreshkin <fennonplay@gmail.com>
2024-05-12 14:56:11 +0500

    123

--- src/components/pages/mainPage/mainPage.jsx
+++ src/components/pages/mainPage/mainPage.jsx
@@ -3,10 +3,12 @@ import Footer from "../../footer/footer";
 import cl from "./mainPage.module.css";
 import mainPageWoman from "../../../images/mainPageWoman.jpg";
 import Navigation from "../../navigation/navigation";
+import Help from "../../popup/help/help";
 
 const MainPage = () => {
     return (
         <div className={cl.mainPage}>
+            <Help/>
             <Navigation/>
             <div className={cl.woman}>
                 <img src={mainPageWoman}/>
