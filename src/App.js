9f02f9f2a21815ee1e0da2ff194842346716c260
ioreshkin <fennonplay@gmail.com>
2024-05-16 19:41:37 +0500

    add pages

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import './reset.css';
 import './components/pages/mainPage/mainPage';
 import Navigation from "./components/navigation/navigation";
 import MainPage from './components/pages/mainPage/mainPage';
+
 import HelpModal from './components/popup/helpModule/helpModal';
 import { useState } from 'react';
+import ProgramsPage from './components/pages/programsPage/programsPage';
+import ExercisesPage from './components/pages/exercisesPage/exercisesPage';
 
 function App() {
   const [helpActive, setHelpActive] = useState(false);
   return (
     <div className="App">
       <Navigation setActive={setHelpActive}/>
-      <MainPage/>
+      {/* <MainPage/> */}
+      <ProgramsPage/>
+      {/* <ExercisesPage/> */}
       <HelpModal active={helpActive} setActive={setHelpActive}/>
     </div>
   );
