98bb03e279667ec0aaf8f8308ac568c8951ac81b
ioreshkin <fennonplay@gmail.com>
2024-05-16 19:35:22 +0500

    create exercisesPage

---
+++ src/components/pages/exercisesPage/exercisesPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import cs from "./exercisesPage.module.css";
+
+const ExercisesPage = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default ExercisesPage;
--- src/components/pages/programsPage/programsPage.jsx
+++ src/components/pages/programsPage/programsPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import cl from "./programsPage.module.css";
 
 const ProgramsPage = () => {
     return (
