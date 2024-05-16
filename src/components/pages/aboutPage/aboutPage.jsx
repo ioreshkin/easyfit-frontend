001cd98ecd69749b83ffe620343ec4a2349f5102
Георгий Куликов <gakulikov@edu.hse.ru>
2024-05-16 21:21:25 +0500

    [eq

--- src/components/pages/aboutPage/aboutPage.jsx
+++ src/components/pages/aboutPage/aboutPage.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import cl from "./aboutPage.module.css";
+import Navigation from "../../navigation/navigation";
+import Footer from "../../footer/footer";
 
-const AboutPage = () => {
+const AboutPage = ({setActive}) => {
     return (
         <div className={cl.aboutPage}>
+            <Navigation setActive={setActive}/>
                 <div className={cl.woman}>
                     <h1>Фитнес-платформа для тренировок дома и в зале</h1>
                 </div>
+                <Footer/>
             </div>
     )
 }
