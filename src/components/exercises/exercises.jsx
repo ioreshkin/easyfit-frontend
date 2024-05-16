d17645dcfd9fe1c942584bcbcb1dddb10694f0b5
Юлия Долгих <yudolgich@gmail.com>
2024-05-16 21:06:02 +0500

    б

--- src/components/exercises/exercises.jsx
+++ src/components/exercises/exercises.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import cl from "./exercises.module.css";
-import Picture from "..//..//images/search exercieses.png"
+import Picture from "../../images/searchexercises.png";
 
 const Exercises = () => {
     return (
         <div>
             <div className={cl.Page}>
               <div className={cl.Search}>
-                <div className={cl.Picture}
-                   <img src={search exercieses} alt="" />
+                <div className={cl.Picture}>
+                   <img src={Picture} alt="" />
+                </div>
+                <div className={cl.Rectangle}></div>
               </div>
-              <div className={cl.Rectangle}></div>
-
             </div>
-            
         </div>
-    )
+    );
 }
 
 export default Exercises;
