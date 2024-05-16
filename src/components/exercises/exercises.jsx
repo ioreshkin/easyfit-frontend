b157c9b5f31926465c345b326f4b3c1ba9d4dd77
Юлия Долгих <yudolgich@gmail.com>
2024-05-16 20:57:59 +0500

    x

--- src/components/exercises/exercises.jsx
+++ src/components/exercises/exercises.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import cl from "./exercises.module.css";
+import Picture from "..//..//images/search exercieses.png"
 
 const Exercises = () => {
     return (
         <div>
-            <div className={cl.Rectangle}>
+            <div className={cl.Page}>
+              <div className={cl.Search}>
+                <div className={cl.Picture}
+                   <img src={search exercieses} alt="" />
+              </div>
+              <div className={cl.Rectangle}></div>
 
             </div>
             
