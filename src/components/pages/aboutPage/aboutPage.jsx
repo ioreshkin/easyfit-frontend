e20e858cd71ebff526c547b84fdc5edeed2ca595
Георгий Куликов <gakulikov@edu.hse.ru>
2024-05-29 17:28:56 +0500

    мвам

--- src/components/pages/aboutPage/aboutPage.jsx
+++ src/components/pages/aboutPage/aboutPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import cl from "./aboutPage.module.css";
 import Footer from "../../footer/footer";
+import weImage from "./we.png";
 
 const AboutPage = ({langCode}) => {
     return (
@@ -16,9 +17,22 @@ const AboutPage = ({langCode}) => {
                 <h2>Easy FIT — это</h2>
                 <div className={cl.stickRectangle}></div>
                 <div className={cl.rects}>
-                <div className={cl.rectangle1}></div> 
-                <div className={cl.rectangle2}></div>
-                <div className={cl.rectangle3}></div>
+                <div className={cl.rectangle1}>
+                    <div className={cl.we.png}>
+                    <h2>Команда</h2>
+                    <p>В нашей команде достаточно опытных людей готовых помочь вам с любым вопросом в трудной ситуации</p>
+                    </div>
+                    </div> 
+                <div className={cl.rectangle2}>
+
+                    <h2>Сборник упражнений</h2>
+                    <p>На нашем сайте собрано подовляющие количество упражнений для каждой тренировки и любого вкуса</p>
+                    </div>
+                <div className={cl.rectangle3}>
+
+                    <h2>Пользователи</h2>
+                    <p>Наши пользователи делают большой вклад, благодаря отзывам, результатам и рекомендациям</p>
+                    </div>
                 </div>
                 <Footer/>
             </div>
