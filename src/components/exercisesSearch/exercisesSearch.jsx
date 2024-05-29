03a5f0cc2e0e206a11a18fc3a0575a2393e6531c
Юлия Долгих <yudolgich@gmail.com>
2024-05-29 19:17:59 +0500

    доделала упражнения

--- src/components/exercisesSearch/exercisesSearch.jsx
+++ src/components/exercisesSearch/exercisesSearch.jsx
@@ -10,42 +10,43 @@ const ExercisesSearch = () => {
         
         <div className={cl.Page}>
             <div className={cl.Rectangle}>
-                 <div className={cl.Picture1}>
-                    <img src={Picture1} alt="" />
-                </div>
-                <div className={cl.Rectangle__info}>
-                    <p>Название упражнения</p>
-                    <span>* описание
-                        * описание
-                        * описание
-                        * описание
-                    </span>
-                    <div className = {cl.ExcInf}>
-                    <span>Тут очень длинное описание упражнения тут очень длинное описание упражнения 
-                        тут очень длинное описание упражнения тут очень длинное описание упражнения 
-                        тут очень длинное описание упражнения тут очень длинное описание упражнения 
-                        тут очень длинное описание упражнения тут очень длинное описание упражнения 
-                        тут очень длинное описание упражнения тут очень длинное описание упражнения
-                    </span>
+                <div className={cl.Start}>
+                    <div className={cl.Picture1}>
+                        <img src={Picture1} alt="" />
                     </div>
-                    <div className={cl.Head}>
+                    <div className={cl.Rectangle__info}>
+                        <p1>Название упражнения</p1>
+                        <span>* описание
+                        <br></br>
+                              * описание
+                        <br></br>
+                              * описание
+                        <br></br>
+                              * описание
+                        </span>
+                    </div>
+                </div>    
+                <div className = {cl.ExcInf}>
+                    <span>Тут очень длинное описание упражнения тут очень длинное описание упражнения тут очень длинное описание упражнения тут очень длинное описание упражнения тут очень длинное описание упражнения тут очень длинное описание упражнения тут очень длинное описание упражнения тут очень длинное описание упражнения тут очень длинное описание упражнения тут очень длинное описание упражнения
+                        </span>
+                 </div>
+                <div className={cl.Head}>
                     <p>Выполнение упражнения</p>
-                    </div>
-                    <div className={cl.Howtodoit}>
-                        <div className={cl.Picture2}>
-                            <img src={Picture2} alt="" />
-                        </div>
-                        <div className={cl.Picture3}>
-                            <img src={Picture3} alt="" />
-                         </div>  
-                    </div>
-                    <div className={cl.Head}>
-                    <p>Видео выполнения упражнения</p>
-                    </div>
-                    <div className={cl.Picture4}>
-                        <img src={Picture4} alt="" />
-                    </div>
                 </div>
+                <div className={cl.Howtodoit}>
+                    <div className={cl.Picture2}>
+                        <img src={Picture2} alt="" />
+                    </div>
+                    <div className={cl.Picture3}>
+                        <img src={Picture3} alt="" />
+                     </div>  
+                 </div>
+                <div className={cl.Head}>
+                    <p>Видео выполнения упражнения</p>
+                </div>
+                <div className={cl.Picture4}>
+                    <img src={Picture4} alt="" />
+                </div>                
             </div>
         </div>
     );
