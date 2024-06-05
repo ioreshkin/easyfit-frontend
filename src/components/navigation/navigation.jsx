c42c5ed4544d0d88798a17461e2ead7038962451
ioreshkin <fennonplay@gmail.com>
2024-06-05 23:28:13 +0500

    fix links

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -10,13 +10,13 @@ const Navigation = ({setActive, langCode, setLangCode}) => {
     const lang = getLang(langCode);
     return (
         <div className={cl.navigation}>
-            <Link className={cl.logo} to="/"><img src={logo} alt=""/></Link>
+            <Link className={cl.logo} to="/" onClick={() => {window.scrollTo(0, 0);}}><img src={logo} alt=""/></Link>
 
             <div className={cl.links}>
-                <Link to={"/programs"}><span>{lang.programs}</span></Link>
-                <Link to={"/exercises"}><span>{lang.exercises}</span></Link>
+                <Link to={"/programs"} onClick={() => {window.scrollTo(0, 0);}}><span>{lang.programs}</span></Link>
+                <Link to={"/exercises"} onClick={() => {window.scrollTo(0, 0);}}><span>{lang.exercises}</span></Link>
                 <Help text={lang.help} setActive={setActive}/>
-                <Link to={"/about"}><span>{lang.about}</span></Link>
+                <Link to={"/about"} onClick={() => {window.scrollTo(0, 0);}}><span>{lang.about}</span></Link>
             </div>
             <LangSwitch langCode={langCode} setLangCode={setLangCode}/>
         </div>
