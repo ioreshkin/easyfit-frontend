f2ffbdd80d60eadf8edab9b07a2bdc4176c8a077
Варвара Осока <varvara.osoka@gmail.com>
2024-06-24 21:10:26 +0500

    zxc

--- src/components/pages/programsInfoPage/programsInfo.jsx
+++ src/components/pages/programsInfoPage/programsInfo.jsx
@@ -4,27 +4,29 @@ import Footer from "../../footer/footer";
 
 const ProgramsInfo = ({info, langCode, data}) => {
     const [exercises, setExercises] = useState([]);
-    const [curerentExercise, setCurrentExercise] = useState(0);
+    const [currentExercise, setCurrentExercise] = useState(0);
     const [repeats, setRepeats] = useState(0);
     const [exerciseName, setExerciseName] = useState("");
     const [exerciseImg, setExerciseImg] = useState("");
     const [exerciseGif, setExerciseGif] = useState("");
+    console.log(info);
+    console.log(data);
 
     let name;
     let shortDescription;
     let description;
     if (langCode == "ru") {name = info.name_ru; shortDescription = info.short_description_ru; description = info.description_ru}
     else if (langCode == "en") {name = info.name_en; shortDescription = info.short_description_en; description = info.description_en}
-    setTimeout(() => {console.log(data); setExercises(data)}, 1);
 
     
     useEffect(() => {
+        setExercises(data)
         if (exercises.length > 0) {
-            setExerciseImg(exercises[curerentExercise].preview);
-            setExerciseGif(exercises[curerentExercise].gif);
-            if (langCode == "ru") {setExerciseName(exercises[curerentExercise].name_ru)}
+            setExerciseImg(exercises[currentExercise].preview);
+            setExerciseGif(exercises[currentExercise].gif);
+            if (langCode == "ru") {setExerciseName(exercises[currentExercise].name_ru)}
         }
-        setRepeats(info.exercises_repeats.split(",\s+")[curerentExercise]);
+        setRepeats(info.exercises_repeats.split(",")[currentExercise]);
  
     }, [exercises])
     return (
@@ -43,16 +45,13 @@ const ProgramsInfo = ({info, langCode, data}) => {
                     <span>{description}</span>
                     <p2>Тренировка состоит из {exercises.length} упражнений, после выполнения каждого нажмите на кнопку “далее”.</p2>
                     <p>Выполнение упражнения</p>
-                    <p3>{curerentExercise + 1}. {exerciseName}</p3>
+                    <p3>{currentExercise + 1}.{exerciseName}</p3>
                     <p4>{repeats} повторений</p4>
-                    {/* <div className={cl.Picture2}>
-                        <img src={exerciseImg} alt="" />
-                    </div> */}
                     <div className={cl.Picture3}>
                         <img src={exerciseGif} alt="" />
                     </div>
-                    <div className={cl.next} onClick={() => {if(exercises[curerentExercise + 1]) {setCurrentExercise(curerentExercise + 1)}}}>
-                        <img src={"/images/later.png"} alt="" />
+                    <div className={cl.next} onClick={() => {if(exercises[currentExercise + 1]) {setCurrentExercise(currentExercise + 1)}}}>
+                        <img src={"/images/later.png"} alt=""/>
                     </div>
                 </div>
             </div>
